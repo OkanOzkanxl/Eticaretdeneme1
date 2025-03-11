2a211fa7abe95bbc35939bb14a55e431841fbd6d
OkanOzkanxl <118337029+OkanOzkanxl@users.noreply.github.com>
2025-03-11 10:53:37 +0300

    Add project files.

---
+++ Eticaret.Core/Entities/Slider.cs
@@ -0,0 +1,13 @@
+﻿namespace Eticaret.Core.Entities
+{
+    public class Slider : IEntity
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+        public string? Link { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+    }
+}
---
+++ Eticaret.Data/DatabaseContext.cs
@@ -0,0 +1,34 @@
+﻿using Eticaret.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.Reflection;
+
+namespace Eticaret.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<News> News { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Slider> Sliders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=LUCIFER-\MSSQLSERVER05; 
+                Database=EticaretDb; Trusted_Connection=True; TrustServerCertificate=True");
+          
+
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); //çalışan dll inclasslarını bulup ayarlıyor
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+    
