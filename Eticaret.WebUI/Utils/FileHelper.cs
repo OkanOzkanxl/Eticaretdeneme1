5b9ead360af3d271efdf21525c7a96f5a29f2654
OkanOzkanxl <118337029+OkanOzkanxl@users.noreply.github.com>
2025-03-20 10:17:37 +0300

    logo hatası aldıgım gün

--- Eticaret.WebUI/Utils/FileHelper.cs
+++ Eticaret.WebUI/Utils/FileHelper.cs
@@ -2,8 +2,7 @@
 {
     public class FileHelper
     {
-        public static async Task<string> FileLoaderAsync(IFormFile formFile,
-            string filePath = "/Img/")
+        public static async Task<string> FileLoaderAsync(IFormFile formFile,string filePath = "/Img/")
         {
             string fileName = "";
             if (formFile != null && formFile.Length > 0) 
@@ -12,11 +11,20 @@
                 string directory = Directory.GetCurrentDirectory() +
                     "/wwwroot" + filePath + fileName;
                 using var stream = new FileStream(directory, FileMode.Create);
-                await stream.CopyToAsync(stream);
+                await formFile.CopyToAsync(stream);
             }
             return fileName;
-
-
+        }
+        public static bool FileRemover(string fileName, string filePath = "/Img/")
+        {
+            string directory = Directory.GetCurrentDirectory() +
+                    "/wwwroot" + filePath + fileName;
+            if (File.Exists(directory))
+            {
+                File.Delete(directory);
+                return true;
+            }
+            return false;
         }
     }
 }
